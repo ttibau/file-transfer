7124ff9888f8a50c1106f1ed9164795010c86c3d
Thiago Tibau <ttibaudev@gmail.com>
2022-06-11 18:15:59 -0300

    React query config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,25 @@ import { GlobalStyles } from '@config/globalStyles';
 import original from 'react95/dist/themes/original';
 import { ThemeProvider } from 'styled-components';
 import AppContextProvider from '@resources/context/AppContext';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={original}>
-        <AppContextProvider>
-          <Component {...pageProps} />
-        </AppContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppContextProvider>
+            <Component {...pageProps} />
+          </AppContextProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
---
+++ resources/hooks/queries/useCreateRoom.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { useMutation } from 'react-query';
+
+const BASE_URL = 'http://localhost:3000/rooms';
+
+const createRoomRequest = async (password: string) => {
+  const { data } = await axios.post(BASE_URL, { password });
+  return data;
+};
+
+export const useCreateRoom = () => {
+  return useMutation(createRoomRequest, {
+    onSuccess: () => {
+      console.log('Room created');
+    },
+  });
+};
